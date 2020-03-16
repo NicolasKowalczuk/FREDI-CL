d002ad4a1b4ffd7e7fe010000e68ba4b8368fc37
Korentin-K <56510443+Korentin-K@users.noreply.github.com>
2020-03-16 15:04:36 +0100

    Add files via upload

---
+++ FREDI-CL-devKoko/FREDI-CL/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace FREDI_CL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.gestionDesLicenciésToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.créerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.visualisermodifierToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gestionDesDéplacementsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.paramètresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.paramètresToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.créerToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.visualisermodifierToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.créerToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.visualisermodifierToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gestionDesLicenciésToolStripMenuItem,
+            this.gestionDesDéplacementsToolStripMenuItem,
+            this.paramètresToolStripMenuItem,
+            this.paramètresToolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1051, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // gestionDesLicenciésToolStripMenuItem
+            // 
+            this.gestionDesLicenciésToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.créerToolStripMenuItem,
+            this.visualisermodifierToolStripMenuItem});
+            this.gestionDesLicenciésToolStripMenuItem.Name = "gestionDesLicenciésToolStripMenuItem";
+            this.gestionDesLicenciésToolStripMenuItem.Size = new System.Drawing.Size(163, 24);
+            this.gestionDesLicenciésToolStripMenuItem.Text = "Gestion des Licenciés";
+            // 
+            // créerToolStripMenuItem
+            // 
+            this.créerToolStripMenuItem.Name = "créerToolStripMenuItem";
+            this.créerToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.créerToolStripMenuItem.Text = "Créer";
+            // 
+            // visualisermodifierToolStripMenuItem
+            // 
+            this.visualisermodifierToolStripMenuItem.Name = "visualisermodifierToolStripMenuItem";
+            this.visualisermodifierToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.visualisermodifierToolStripMenuItem.Text = "Visualiser/modifier";
+            // 
+            // gestionDesDéplacementsToolStripMenuItem
+            // 
+            this.gestionDesDéplacementsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.créerToolStripMenuItem1,
+            this.visualisermodifierToolStripMenuItem1});
+            this.gestionDesDéplacementsToolStripMenuItem.Name = "gestionDesDéplacementsToolStripMenuItem";
+            this.gestionDesDéplacementsToolStripMenuItem.Size = new System.Drawing.Size(199, 24);
+            this.gestionDesDéplacementsToolStripMenuItem.Text = "Gestion des Déplacements";
+            // 
+            // paramètresToolStripMenuItem
+            // 
+            this.paramètresToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.créerToolStripMenuItem2,
+            this.visualisermodifierToolStripMenuItem2});
+            this.paramètresToolStripMenuItem.Name = "paramètresToolStripMenuItem";
+            this.paramètresToolStripMenuItem.Size = new System.Drawing.Size(193, 24);
+            this.paramètresToolStripMenuItem.Text = "Gestion des notes de frais";
+            // 
+            // paramètresToolStripMenuItem1
+            // 
+            this.paramètresToolStripMenuItem1.Name = "paramètresToolStripMenuItem1";
+            this.paramètresToolStripMenuItem1.Size = new System.Drawing.Size(96, 24);
+            this.paramètresToolStripMenuItem1.Text = "Paramètres";
+            // 
+            // créerToolStripMenuItem1
+            // 
+            this.créerToolStripMenuItem1.Name = "créerToolStripMenuItem1";
+            this.créerToolStripMenuItem1.Size = new System.Drawing.Size(224, 26);
+            this.créerToolStripMenuItem1.Text = "Créer";
+            // 
+            // visualisermodifierToolStripMenuItem1
+            // 
+            this.visualisermodifierToolStripMenuItem1.Name = "visualisermodifierToolStripMenuItem1";
+            this.visualisermodifierToolStripMenuItem1.Size = new System.Drawing.Size(224, 26);
+            this.visualisermodifierToolStripMenuItem1.Text = "Visualiser/modifier";
+            // 
+            // créerToolStripMenuItem2
+            // 
+            this.créerToolStripMenuItem2.Name = "créerToolStripMenuItem2";
+            this.créerToolStripMenuItem2.Size = new System.Drawing.Size(224, 26);
+            this.créerToolStripMenuItem2.Text = "Créer";
+            // 
+            // visualisermodifierToolStripMenuItem2
+            // 
+            this.visualisermodifierToolStripMenuItem2.Name = "visualisermodifierToolStripMenuItem2";
+            this.visualisermodifierToolStripMenuItem2.Size = new System.Drawing.Size(224, 26);
+            this.visualisermodifierToolStripMenuItem2.Text = "Visualiser/modifier";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1051, 505);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem gestionDesLicenciésToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem créerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem visualisermodifierToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gestionDesDéplacementsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem créerToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem visualisermodifierToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem paramètresToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem créerToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem visualisermodifierToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem paramètresToolStripMenuItem1;
+    }
+}
+
